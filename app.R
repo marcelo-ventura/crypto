38cc20f70098c4590d551b3bbbebf4badab01399
Marcelo Ventura <marcelos.ventura@gmail.com>
2018-05-27 11:01:19 -0300

    my app

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shinythemes)
+library(RcppArmadillo)
 library(shiny)
 library(rvest)
 library(httr)
@@ -10,7 +11,7 @@ library(GeneralizedHyperbolic)
 library(forecast)
 library(rsconnect)
 
-
+options(scipen=999)
 # Define UI 
 ui <- shinyUI(fluidPage(theme=shinytheme("superhero"),
                         
@@ -19,11 +20,11 @@ ui <- shinyUI(fluidPage(theme=shinytheme("superhero"),
                         
                         # Sidebar with a slider input for number of bins 
                         mainPanel(
-                          textInput("cripto",label = h3("Type your favorite coin :)"))
+                          textInput("cripto",label = h3("Type your favorite crypto coin :)"))
                           # Show a plot of the coin
                           ,
                           selectInput("select", label = h3("Variable select"), 
-                                      choices = list("Highest price" = 3, "Lowest price" = 4, "Close price" = 5,
+                                      choices = list("Highest price" = 3, "Lowest price" = 4, "Closing price" = 5,
                                                      "Volume" = 6, "Marketcap" = 7), 
                                       selected = 4),
                           
@@ -37,10 +38,10 @@ ui <- shinyUI(fluidPage(theme=shinytheme("superhero"),
 server <- function(input, output){
   
   output$coinplot <- renderPlot({
-    titulo.moeda <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    titulo.moeda <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_node("title") %>%
       html_text()
-    preco.dia <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    preco.dia <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr *.text-left") %>%
       html_text() %>%
       as.character() %>%
@@ -54,31 +55,31 @@ server <- function(input, output){
       as.Date(.,format="%b %d, %Y")%>%
       as.data.frame()
     
-    preco.abertura <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    preco.abertura <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(2)") %>%
       html_text() %>%
       as.numeric()%>%
       as.data.frame()
     
-    preco.maximo <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    preco.maximo <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(3)") %>%
       html_text() %>%
       as.numeric()%>%
       as.data.frame()
     
-    preco.minimo <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    preco.minimo <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(4)") %>%
       html_text() %>%
       as.numeric()%>%
       as.data.frame()
     
-    preco.fechamento <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    preco.fechamento <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(5)") %>%
       html_text() %>%
       as.numeric()%>%
       as.data.frame()
     
-    volume.dia <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    volume.dia <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(6)") %>%
       html_text() %>%
       { gsub(",","", .) } %>%
@@ -86,7 +87,7 @@ server <- function(input, output){
       as.data.frame()
     
     
-    capitaliz.mercado <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180313"))%>%
+    capitaliz.mercado <- read_html(paste0("https://coinmarketcap.com/currencies/",as.character(input$cripto),"/historical-data/?start=20130428&end=20180424"))%>%
       html_nodes("tbody tr td:nth-child(7)") %>%
       html_text() %>%
       { gsub(",","", .) } %>%
@@ -94,7 +95,7 @@ server <- function(input, output){
       as.data.frame()
     
     moeda <- cbind(preco.dia,preco.abertura,preco.maximo,preco.minimo,preco.fechamento,volume.dia,capitaliz.mercado)%>%
-      `colnames<-`(c("Day","Opening price", "Highest price", "Minimum price", "Close price", "Volume", "Marketcap"))
+      `colnames<-`(c("Day","Opening price(USD)", "Highest price(USD)", "Lowest price(USD)", "Closing price(USD)", "Volume(USD)", "Marketcap(USD)"))
     
     nomedacolunaescolhida <- as.character(colnames(moeda)[as.numeric(input$select)])
     ggplot(moeda, aes(Day)) + geom_line(aes(y=moeda[,as.numeric(input$select)])) + theme_economist() + theme(axis.title.x = element_blank())+labs(y=nomedacolunaescolhida)+ggtitle(titulo.moeda)
@@ -105,11 +106,4 @@ server <- function(input, output){
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-# Publish the application
-
-rsconnect::setAccountInfo(name='marcelo-ventura',
-                          token='E5219D51D9B8B7CBC4CCE6FBFB0AC8DF',
-                          secret='yxLk+9Uw4e3mC84da34yKDgvsIq4G8H3kLfeEdKN')
-rsconnect::deployApp('~OneDrive/github/crypto/cryptoapp(beta).R')
 
