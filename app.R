a078f52a93432a074eb3c7647e22dcd61da95f2a
Marcelo Ventura <marcelos.ventura@gmail.com>
2018-06-07 18:54:23 -0300

    my crypto app

--- app.R
+++ app.R
@@ -15,28 +15,26 @@ options(scipen=999)
 # Define UI 
 ui <- shinyUI(fluidPage(theme=shinytheme("superhero"),
                         
-                        # Application title
-                        headerPanel("Live webscrapper from coinmarketcap.com"),
+          # Application title
+          headerPanel("Live webscrapper from coinmarketcap.com"),
                         
-                        # Sidebar with a slider input for number of bins 
-                        sidebarPanel(
-                          textInput("cripto",label = h3("Type your favorite crypto coin :)")),
-                          checkboxInput("log","Work with log-variables",value = T),
-                          textInput("interval",label = h3("Time interval (eg: 20160531-20160630, or all time)"),value="all time"),
-                          # Show a plot of the coin
-                          selectInput("select", label = h3("Variable select"), 
-                                      choices = list("Opening price"=2,"Highest price" = 3, "Lowest price" = 4, "Closing price" = 5,
+          # Sidebar with a slider input for number of bins 
+          sidebarPanel(
+          textInput("cripto",label = h3("Type your favorite crypto coin :)")),
+          checkboxInput("log","Work with log-variables",value = T),
+          textInput("interval",label = h3("Time interval (eg: 20160531-20160630, or all time)"),value="all time"),
+          # Show a plot of the coin
+          selectInput("select", label = h3("Variable select"), 
+                      choices = list("Opening price"=2,"Highest price" = 3, "Lowest price" = 4, "Closing price" = 5,
                                                      "Volume" = 6, "Marketcap" = 7), 
                                       selected = 4),
-                          
-                          hr(),
-                          fluidRow(column(3, verbatimTextOutput("value")))),
-                          mainPanel(
-                            tabsetPanel(type="tabs",
-                                  tabPanel("Real data",plotOutput("coinplot")),
-                                  tabPanel("Adjusted data",plotOutput("plotadjusted")),
-                                  tabPanel("Adjusted vs. real",plotOutput("plotadjreal"))))
-                        ))
+                                      hr(),
+                      fluidRow(column(3, verbatimTextOutput("value")))),
+          mainPanel(
+                    tabsetPanel(type="tabs",
+                    tabPanel("Real data",plotOutput("coinplot")),
+                    tabPanel("Adjusted data",plotOutput("plotadjusted")),
+                    tabPanel("Adjusted vs. real",plotOutput("plotadjreal"))))))
 
 # Define server logic required to draw the plot
 server <- function(input, output){
@@ -47,10 +45,7 @@ server <- function(input, output){
                                  "/historical-data/?start=20130428&end=",format(Sys.Date(),"%Y%m%d")))}else{
     read_html(paste0("https://coinmarketcap.com/currencies/",
                      input$cripto,
-                     "/historical-data/?start=",substr(input$interval,1,8),"&end=",substr(input$interval,10,17)))
-      
-                                 }
-                      })
+                     "/historical-data/?start=",substr(input$interval,1,8),"&end=",substr(input$interval,10,17)))}})
     moeda<- reactive({data.frame('Day'=paginalida()%>%html_nodes("tbody tr *.text-left")%>%html_text() %>%
                       as.character() %>%
                       gsub("Feb","Fev",.) %>%
@@ -74,43 +69,71 @@ server <- function(input, output){
              as.numeric())%>%
     mutate('Marketcap(USD)'=paginalida()%>%html_nodes("tbody tr td:nth-child(7)") %>%html_text() %>%
              gsub(",","", .)%>%
-             as.numeric())
-    })
+             as.numeric())%>%
+        arrange(Day) %>%
+        na.omit()
+      })
     
   output$coinplot <- renderPlot({
+                                 ggplot(data=moeda(), aes(Day)) + 
+                                        geom_line(aes(y=moeda()[,as.numeric(input$select)])) + 
+                                        theme_economist() + 
+                                        theme(axis.title.x = element_blank())+
+                                        labs(y="USD")+
+                                        ggtitle(paste0("Time series of"," ",input$cripto)
+                                                )
+                                 })
+  
+  valores.ajustados <- reactive(hyperblm(`Closing price(USD)`~ `Highest price(USD)`+
+                                                            `Lowest price(USD)`+
+                                                            `Volume(USD)`+
+                                                            `Marketcap(USD)`,
+                                                             data=moeda()
+                                          )$fitted.values
+                                )
+                      
+                     
+  DATA<-reactive(data.frame(
+                            rbind(data.frame('dia'=1:nrow(moeda()),
+                                             'id'=rep("reais",nrow(moeda())),
+                                             'vetor'=moeda()$`Closing price(USD)`
+                                             )
+                                  ,
+                                  data.frame('dia'=1:nrow(moeda()),
+                                             'id'=rep("ajustados",nrow(moeda())),
+                                             'vetor'=valores.ajustados()
+                                             )
+                                  )
+                            )
+                 )
+                 
+                 
+  
+  output$plotadjusted <- renderPlot(
+                                    ggplot(data=(filter(DATA(),id=="ajustados")), aes(x=dia, y=vetor, group=id, colour=id))+
+                                           geom_line(size=1.15) + 
+                                           theme_economist() + 
+                                           labs(x="Number of days",y="Prices") + 
+                                           scale_colour_manual(values = c("Yellow"),
+                                                               labels = c("Adjusted")
+                                                               )+
+                                           ggtitle(paste0("Adjusted data:"," ",input$cripto))
+                                              
+                                    )
+  output$plotadjreal <- renderPlot(
+                                   ggplot(data=DATA(), aes(x=dia,y=vetor,group=id,colour=id)) + 
+                                           geom_line(size=1.15) + 
+                                           theme_economist() + 
+                                           theme(axis.title.x = element_blank())+
+                                           labs(y="",x="Number of days")+
+                                           scale_colour_manual(values = c("Red","Yellow"), 
+                                                         labels = c("Real", "Adjusted"))+
+                                           ggtitle(paste0("Real data vs adjusted:"," ",input$cripto))
     
-    ggplot(data=moeda(), aes(Day)) + geom_line(aes(y=moeda()[,as.numeric(input$select)])) + 
-      theme_economist() + theme(axis.title.x = element_blank())+
-      labs(y="USD")+ggtitle(paste0("Time series of"," ",input$cripto))
-    
-})
-  
-  #modelo <- hyperblm(moeda()$`P. fechamento`~moeda()$`P. maximo`+moeda()$`P. minimo`+moeda()$Volume+moeda()$`Cap. mercado`)
-  #coeficientes<-modelo$coefficients
-  #params<- modelo$distributionParams
-  #residuo<-moeda()$`P. fechamento` - ajustados
-  
-  #REAIS<-data.frame('dia'=1:nrow(moeda()),'id'=rep("reais",nrow(moeda())),'reais'=moeda()$`P. fechamento`)
-  #AJUSTADOS<-data.frame('dia'=1:nrow(moeda()),'id'=rep("ajustados",nrow(moeda())),'ajustados'=moeda()$fitted.values)
-  
-  #DATA<-rbind(REAIS,AJUSTADOS)
-  
-  #output$plotadjusted <- renderPlot(
-   # ggplot(AJUSTADOS, aes(x=dia, y=vetor, group=id, colour=id))+ 
-    #  geom_line(size=1.15) + xlab("Registro - Dias") + ylab("Preços") + 
-     # scale_colour_manual(values = c("Yellow"),
-      #                    name=" ", labels = c("Valores Ajustados"))
-       #)
-  #output$plotadjreal <- renderPlot(
-   # ggplot(DATA, aes(x=dia, y=vetor, group=id, colour=id))+
-    #  grafico + geom_line(size=0.5) + xlab("Registro - Dias") + 
-     # ylab("Preços") + scale_colour_manual(values = c("Red","Yellow"),
-      #                                     name="Comparação", labels = c("Valores Reais", "Valores Ajustados"))
-    #)
-  
+                                    
+      )
   
 }
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
